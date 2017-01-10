8956f4cef5e7e5d4f607ecdf50f4b9c7bae27205
Brynna McCarter and Koji Nakagawa <brynna@>
2017-01-10 15:32:00 -0800

    update jquery and remove red border

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,5 @@
 $(function(){
 
-//  $(".panel-title").click(function(){
-//    $(".initially-showing").toggle();
-//    $(".initially-hidden").toggle();
-//  });
-
   $(".panel-body").click(function(){
     $(this).children().toggle();
   });
